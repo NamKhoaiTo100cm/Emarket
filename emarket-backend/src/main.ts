0e5a981a9de4591a9d65f37971c381491bbc3a61
Nam Nguyen <namnguyen301003@gmail.com>
2026-06-18 01:14:29 +0700

    modify prefix

--- emarket-backend/src/main.ts
+++ emarket-backend/src/main.ts
@@ -7,6 +7,7 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true, // 🔥 convert string -> number
